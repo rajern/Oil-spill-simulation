727d0e4427a614412cc29e1cb23ec72693f53ba7
Elias Sigurd Kroken <elias.sigurd.kroken@nmbu.no>
2025-01-07 09:19:54 +0100

    added a "sketch" of a simulation

---
+++ simulation.py
@@ -0,0 +1,12 @@
+from msh_classes import *
+import meshio as m
+import numpy as np
+
+msh_name = ./input_data/bay.msh
+msh = m.read(msh_name)
+
+mesh_instance = mesh(msh)
+mesh_instance.find_neighbors()
+
+#initial point
+spill_point = [0.35, 0.45]
