665aad7167ea19594d5a2ced2595bc71040aabd8
Rajvir <rajvir.singh.aujla@nmbu.no>
2025-01-07 13:40:25 +0100

    space

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     mesh_instance.find_neighbors()
 
     #bay point:
-    x, y=0.45, 0.35
+    x, y = 0.45, 0.35
 
     print(f"The point ({x}, {y}) is inside Triangle {mesh_instance.point_in_triangle(x, y)}")
 
