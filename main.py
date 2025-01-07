a39595d33c1bed077b68c983e71ecfd8a661ab5f
Rajvir <rajvir.singh.aujla@nmbu.no>
2025-01-07 13:44:55 +0100

    remove comment

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from msh_classes import *
 import meshio as m
 import numpy as np
 
-"""
-msh_name = "./input_data/bay.msh"
-msh = m.read(msh_name)
-
-mesh_instance = mesh(msh)
-mesh_instance.find_neighbors()
-"""
 if __name__ == "__main__":
     msh_name = "./input_data/bay.msh"
     msh = m.read(msh_name)
