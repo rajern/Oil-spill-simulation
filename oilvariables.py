117ab75db6305c02aa6bd4362b1cf5def82539d3
Rajvir <rajvir.singh.aujla@nmbu.no>
2025-01-21 09:46:03 +0100

    Renamed file and delete files

--- oilvariables.py
+++
@@ -1,5 +0,0 @@
-# u(t, x) = amount of oil at a given position x at time t
-# u(t=0, x) = initial amount of oil at a given position x = exp(||(-x-xs)/0.01||)
-
-def u(t=0, x):
-    return np.exp(np.linalg.norm((-x-xs)/0.01))
