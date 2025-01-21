8adfe01eb510669d93f0dcaf5ed3de9bd07698a5
Elias Sigurd Kroken <elias.sigurd.kroken@nmbu.no>
2025-01-21 11:45:22 +0100

    added toml file and read toml file

---
+++ readToml.py
@@ -0,0 +1,60 @@
+import toml
+import os
+
+class ConfigReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.config = None
+
+    def load_config_file(self):
+        if not os.path.exists(self.file_path):
+            raise FileNotFoundError(f'Config file {self.file_path} does not exist.')
+        with open(self.file_path, 'r') as file: 
+            self.config = toml.load(file)
+
+        self.validate_config()
+
+    def validate_config(self):
+        '''
+        Validate TOML file to fulfill all requirements. 
+        '''
+        required_sections = ['settings', 'geometry', 'IO']
+        for section in required_sections:
+            if section not in self.config: 
+                raise ValueError(f'{self.config} is missing required section: {section}.')
+            
+        # Validate settings section
+        settings = self.config['settings']
+        if 'nSteps' not in settings:
+            raise ValueError('nSteps is not provided.')
+        if 'tStart' not in settings: 
+            settings['tStart'] = 0
+        if 'tEnd' not in settings: 
+            raise ValueError('tEnd is not provided.')
+        
+
+        # Validate geometry section
+        geometry = self.config['geometry']
+        if 'meshName' not in geometry or 'borders' not in geometry: 
+            raise ValueError('Missing keys in geometry section.')
+        
+        # Validate IO section
+        IO = self.config['IO']
+        if 'logName' not in IO:
+            IO['logName'] = 'logfile'
+
+        restart_file = 'restartFile' in IO and IO['restartFile']
+        start_time = 'tStart' in settings and settings['tStart'] > 0
+
+        if restart_file and not start_time:
+            raise ValueError('If restart file is provided start time must also be.')
+        if start_time and not restart_file: 
+            raise ValueError('If start time is provided restart file must also be.')
+
+
+        write_frequency = IO.get('writeFrequency', 0) # Sets writeFrequency to 0 if it is undefined
+        if not isinstance(write_frequency, (int, float)) or write_frequency <= 0: 
+            print('"writeFrequency" is not provided or is unvalid. No video will be recorded.')
+        
+    def get_value(self, section, key):
+        return self.config[section].get(key) # config['settings'].get('nSteps') = 500 for example
