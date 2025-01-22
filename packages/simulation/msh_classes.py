0e2d8e146fa92d518b7742d42230d0829d3b3221
Rajvir <rajvir.singh.aujla@nmbu.no>
2025-01-22 11:04:27 +0100

    Minor changes

--- packages/simulation/msh_classes.py
+++ packages/simulation/msh_classes.py
@@ -201,9 +201,8 @@ class Mesh:
         A function that stores the neighbors of the cells in the mesh in the _cells list.
         Uses the store_neighbors function.
         """
-        for cell in self._cells:
+        for cell in self._cells:                
             if isinstance(cell, Triangle):
-            
                 cell.store_neighbors(self._cells)
-
+            
         print("Neighbors for cells computed")
